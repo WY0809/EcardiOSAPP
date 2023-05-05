353c631fdcd07fac5a053e1cd4107f8e3d43bb72
Wenyao <mars36549@gmail.com>
2023-05-05 17:47:29 +0800

    ver1

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -6,16 +6,40 @@
 //
 
 import SwiftUI
+import Firebase
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack{
+            Button("Register"){
+                Auth.auth().createUser(withEmail: "cind@test.com", password: "1234567") { (Result, Error) in
+                    guard Error == nil else{
+                        return
+                    }
+                    print("OK")
+                }
+                let changeRequest = Auth.auth().currentUser?.createProfileChangeRequest()
+                changeRequest?.displayName = "帥哥"
+                changeRequest?.commitChanges(completion: { error in
+                    guard error == nil else {
+                        print(error?.localizedDescription)
+                        return
+                    }
+                })
+            }
+            Button("Check"){
+                
+                if let user = Auth.auth().currentUser {
+                    print(user.uid, user.email, user.displayName)
+                }
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .previewInterfaceOrientation(.landscapeRight)
     }
 }
