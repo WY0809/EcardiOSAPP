a6e1295790fc4a9b91cd4b68ac0146e4ce726999
Wenyao <mars36549@gmail.com>
2023-05-05 16:40:24 +0800

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by 曹文耀 on 2023/5/5.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/E_cardApp.swift
@@ -0,0 +1,17 @@
+//
+//  E_cardApp.swift
+//  Shared
+//
+//  Created by 曹文耀 on 2023/5/5.
+//
+
+import SwiftUI
+
+@main
+struct E_cardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
